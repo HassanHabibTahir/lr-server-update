cd4577e0b12e3b57e5f74f4333daeebc6b8886d5
HassanHabibTahir <hassanhabibtahir@gmail.com>
2024-07-03 09:03:04 +0500

    fix file issue

--- src/helpers/storageUtil.js
+++ src/helpers/storageUtil.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+// const getStoragePath = (file) => {
+//   const storagePath = path.join(__dirname, '../', '/storage/images',JSON.stringify(file?.name)); 
 
+//   // // Ensure the storage path directory exists
+//   // if (!fs.existsSync(storagePath)){
+//   //   fs.mkdirSync(storagePath, { recursive: true });
+//   // }
+
+//   return storagePath;
+// };
 const getStoragePath = (file) => {
-  const storagePath = path.join(__dirname, '../', '/storage/images',JSON.stringify(file?.name)); 
-
-  // // Ensure the storage path directory exists
-  // if (!fs.existsSync(storagePath)){
-  //   fs.mkdirSync(storagePath, { recursive: true });
-  // }
-
+  const tempDir = os.tmpdir();
+  const storagePath = path.join(tempDir, file.name);
   return storagePath;
 };
-
 module.exports = {
   getStoragePath,
 };
